b23877a20cfb9d83f00393d8a27d55549e8e6011
Abhirup <abhidatta146@gmail.com>
2022-03-03 15:37:43 +0530

    added basic code of layout

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import {
   View,
   Text,
+  StyleSheet,
 } from 'react-native';
-
+import BottomComponent from './src/components/BottomComponent';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello </Text>
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      <BottomComponent />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'pink'
+  }
+})
+
 
 export default App;
---
+++ src/components/BottomComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+  Text,
+}from 'react-native';
+
+const BottomComponent = ()=>{
+  return <View style={{borderWidth:1}}>
+      <Text>BottomComponent</Text>
+  </View>
+}
+
+export default BottomComponent;
