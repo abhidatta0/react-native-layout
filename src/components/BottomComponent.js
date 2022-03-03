5de01f332f30de3a319a088ae1a442a321b8b504
Abhirup <abhidatta146@gmail.com>
2022-03-03 20:47:10 +0530

    absolute and relative container styles

--- src/components/BottomComponent.js
+++ src/components/BottomComponent.js
@@ -2,12 +2,31 @@ import React from 'react';
 import {
   View,
   Text,
+  StyleSheet,
 }from 'react-native';
 
 const BottomComponent = ()=>{
-  return <View style={{borderWidth:1}}>
+  {/*  try using  style.relativeContainer instead  */}
+  return <View style={style.relativeContainer}> 
       <Text>BottomComponent</Text>
   </View>
 }
+
+const style = StyleSheet.create({
+  absoluteContainer:{
+    position: 'absolute',
+    borderWidth: 1,
+    bottom: 1,
+    top: 1,
+    left:5,
+  },
+  relativeContainer:{
+    position: 'relative',
+    borderWidth: 1,
+    // bottom: 10,
+    top: 10,
+    left:5,
+  }
+})
 
 export default BottomComponent;
